30e090ba4317473a17e53293296416a7aaa3c807
Kunwardivassingh <120042875+Kunwardivassingh@users.noreply.github.com>
2024-07-14 22:48:24 +0530

    Update buuy.php

--- buuy.php
+++ buuy.php
@@ -3,11 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Check if the form is submitted
-//if ($_SERVER["REQUEST_METHOD"] == "POST" )
-//if (isset($_POST["buy"])) {
-    // data from the form
-   // Process the form data
+
    $fullName = $_POST["name"];
    $email = $_POST["email"];
    $address = $_POST["address"];
